8cf387b5f6a8c092d17f87d18ac721956a891eff
Herc <schavez13371337@yahoo.com>
2023-04-11 12:45:34 -0700

    041123-12:45pm NAVBUTTON

--- cokesite2/src/app/navbar/navbar.component.ts
+++ cokesite2/src/app/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, Renderer2, ElementRef } from '@angular/core';
+import { Component, Renderer2, ElementRef, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -6,17 +6,20 @@ import { Component, Renderer2, ElementRef } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
-  
+  constructor() {}
   isListVisible: boolean = false;
-
-  toggleListDisplay() {
-    this.isListVisible = !this.isListVisible;
-  }
-  
+    toggleListDisplay() {
+      this.isListVisible = !this.isListVisible;
+    }
+  @HostListener('window:resize', ['$event'])
+    onWindowResize(event: any) {
+      if (event.target.innerWidth > 600) {
+        this.isListVisible = false;
+      }
+    }
   isRed: boolean = false;
-
-  toggleColor() {
-    this.isRed = !this.isRed;
-  }
+    toggleColor() {
+      this.isRed = !this.isRed;
+    }
 }
 
