90db366a7cd237272bdae0920a3272fbd01e8937
Herc <schavez13371337@yahoo.com>
2023-04-11 11:58:57 -0700

    041123-11:58pm MOBILEresponseDONE

--- cokesite2/src/app/navbar/navbar.component.ts
+++ cokesite2/src/app/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
+  
+  isListVisible: boolean = false;
 
+  toggleListDisplay() {
+    this.isListVisible = !this.isListVisible;
+  }
+  
+  isRed: boolean = false;
+
+  toggleColor() {
+    this.isRed = !this.isRed;
+  }
 }
+
